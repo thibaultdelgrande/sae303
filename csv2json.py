ba0fb8d9405aaff12c9755bc055aa2a1fc8c1faa
Thibault Delgrande <contact@thibaultdg.fr>
2022-12-15 08:51:34 +0100

    incoyarble commit

---
+++ csv2json.py
@@ -0,0 +1,18 @@
+#codename: utf-8
+
+import csv
+import json
+
+
+
+def csv2json(csvfile):
+    with open(csvfile, 'r') as f:
+        reader = csv.DictReader(f, delimiter=';')
+        rows = list(reader)
+        jsonfile = csvfile.replace('.csv', '.json')
+        with open(jsonfile, 'w') as f:
+            json.dump(rows, f, indent=4)
+        print('Converted {} to {}'.format(csvfile, jsonfile))
+
+if __name__ == '__main__':
+    csv2json('data/commune.csv')
